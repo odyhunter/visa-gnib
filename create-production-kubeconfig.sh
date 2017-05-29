1fc1a59a2d1fc304a840b2e5f0e95bbecb7cc4d2
Alex Z <zamaialexander@gmail.com>
2017-05-29 22:04:39 +0200

    trying tag version injection into k8s config

--- create-production-kubeconfig.sh
+++ create-production-kubeconfig.sh
@@ -1,35 +1,40 @@
 #!/bin/bash
 
-gcloud container clusters get-credentials prod-visa-gnib --zone europe-west1-c
+read -p "Please enter your CLUSTER_NAME: " cluster_name
+read -p "Please enter your cluster ZONE: " zone
 
-SERVER=$(gcloud container clusters describe prod-visa-gnib	\
+gcloud container clusters get-credentials $cluster_name --zone europe-west1-d --project visa-gnib-notifier
+
+SERVER=$(gcloud container clusters describe $cluster_name --zone $zone	\
   --format 'value(endpoint)')
-CERTIFICATE_AUTHORITY_DATA=$(gcloud container clusters describe prod-visa-gnib	 \
+CERTIFICATE_AUTHORITY_DATA=$(gcloud container clusters describe $cluster_name --zone $zone	 \
   --format 'value(masterAuth.clusterCaCertificate)')
-CLIENT_CERTIFICATE_DATA=$(gcloud container clusters describe prod-visa-gnib	 \
+CLIENT_CERTIFICATE_DATA=$(gcloud container clusters describe $cluster_name --zone $zone	 \
   --format 'value(masterAuth.clientCertificate)')
-CLIENT_KEY_DATA=$(gcloud container clusters describe prod-visa-gnib	 \
+CLIENT_KEY_DATA=$(gcloud container clusters describe $cluster_name --zone $zone	 \
   --format 'value(masterAuth.clientKey)')
-kubectl config set-cluster gke --kubeconfig prod-visa-gnib.kubeconfig
+
+kubectl config set-cluster gke --kubeconfig $cluster_name.kubeconfig
 kubectl config set clusters.gke.server \
   "https://${SERVER}" \
-  --kubeconfig prod-visa-gnib.kubeconfig
+  --kubeconfig $cluster_name.kubeconfig
+
 kubectl config set clusters.gke.certificate-authority-data \
   ${CERTIFICATE_AUTHORITY_DATA} \
-  --kubeconfig prod-visa-gnib.kubeconfig
-kubectl config set-credentials cloudbuilder --kubeconfig prod-visa-gnib.kubeconfig
+  --kubeconfig $cluster_name.kubeconfig
+kubectl config set-credentials cloudbuilder --kubeconfig $cluster_name.kubeconfig
 kubectl config set users.cloudbuilder.client-certificate-data \
   ${CLIENT_CERTIFICATE_DATA} \
-  --kubeconfig prod-visa-gnib.kubeconfig
+  --kubeconfig $cluster_name.kubeconfig
 kubectl config set users.cloudbuilder.client-key-data \
   ${CLIENT_KEY_DATA} \
-  --kubeconfig prod-visa-gnib.kubeconfig
+  --kubeconfig $cluster_name.kubeconfig
 
-kubectl config set-context cloudbuilder \
-  --cluster=gke \
-  --user=cloudbuilder \
-  --kubeconfig prod-visa-gnib.kubeconfig
-kubectl config use-context cloudbuilder \
-  --kubeconfig prod-visa-gnib.kubeconfig
+#kubectl config set-context cloudbuilder \
+#  --cluster=gke \
+#  --user=cloudbuilder \
+#  --kubeconfig $cluster_name.kubeconfig
+#kubectl config use-context cloudbuilder \
+#  --kubeconfig $cluster_name.kubeconfig
 
 #gsutil cp production.kubeconfig gs://hightowerlabs/production.kubeconfig
